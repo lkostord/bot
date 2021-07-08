a73c9d279a7dee7bba7b8b7d23045029fb8f0bc9
lkostord <1234dfg96@mail.ru>
2021-07-08 15:24:51 +0300

    'test2'

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import telebot
 
 bot = telebot.TeleBot('AAGqvomcrkJ5CC6-hSePdZAYURIXS72C6-4')
-
 @bot.message_handler(commands=['start' , 'help'])
-
 def send_welcome(message):
     bot.reply_to(message, f'Привет, мы будем изучать новые слова')
+
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
@@ -13,5 +12,6 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, 'Привет!')
     else:
         bot.send_message(message.from_user.id, 'не понимаю, что это значит')
+
 
 bot.polling(none_stop=True)
